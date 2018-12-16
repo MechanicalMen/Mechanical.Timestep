9b2c8552dbecdb15089ffba870cd41cc9b9718ff
TastyBytes <mechanicalmen.tastybytes@gmail.com>
2018-12-16 13:22:50 +0100

    Basic code for updating position and velocity, based on acceleration and elapsed time (in 1D).

---
+++ source/Mechanical.Timestep.Tests/TimestepMathTests.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+
+namespace Mechanical.Timestep.Tests
+{
+    [TestFixture]
+    public static class TimestepMathTests
+    {
+        [Test]
+        public static void UpdatePosition()
+        {
+            double position = 0;
+            double velocity = 0;
+            double acceleration = 10;
+            var dt = new ElapsedTime(seconds: 1d);
+
+            TimestepMath.UpdatePosition(ref position, ref velocity, acceleration, dt);
+            Assert.AreEqual(10, position);
+            Assert.AreEqual(10, velocity);
+
+            TimestepMath.UpdatePosition(ref position, ref velocity, acceleration, dt);
+            Assert.AreEqual(30, position);
+            Assert.AreEqual(20, velocity);
+
+            TimestepMath.UpdatePosition(ref position, ref velocity, acceleration, dt);
+            Assert.AreEqual(60, position);
+            Assert.AreEqual(30, velocity);
+
+            // reproduce using overload
+            position = velocity = 0;
+            for( int i = 0; i < 3; ++i )
+                (position, velocity) = TimestepMath.UpdatePosition(position, velocity, acceleration, dt);
+            Assert.AreEqual(60, position);
+            Assert.AreEqual(30, velocity);
+        }
+    }
+}
---
+++ source/Mechanical.Timestep/TimestepMath.cs
@@ -0,0 +1,52 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Mechanical.Timestep
+{
+    /// <summary>
+    /// Basic math utility functions. 
+    /// </summary>
+    public static class TimestepMath
+    {
+        #region UpdatePosition
+
+        /// <summary>
+        /// Updates position and velocity (using the semi-implicit euler method).
+        /// </summary>
+        /// <param name="position">The position to update.</param>
+        /// <param name="velocity">The velocity to update.</param>
+        /// <param name="acceleration">The current acceleration.</param>
+        /// <param name="dt">The time elapsed.</param>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void UpdatePosition(
+            ref double position,
+            ref double velocity,
+            double acceleration,
+            ElapsedTime dt )
+        {
+            velocity += acceleration * dt.Seconds;
+            position += velocity * dt.Seconds;
+        }
+
+        /// <summary>
+        /// Updates position and velocity (using the semi-implicit euler method).
+        /// </summary>
+        /// <param name="currentPosition">The current position.</param>
+        /// <param name="currentVelocity">The current velocity.</param>
+        /// <param name="acceleration">The current acceleration.</param>
+        /// <param name="dt">The time elapsed.</param>
+        /// <returns>The updated position and velocity.</returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static (double position, double velocity) UpdatePosition(
+            double currentPosition,
+            double currentVelocity,
+            double acceleration,
+            ElapsedTime dt )
+        {
+            var newVelocity = currentVelocity + acceleration * dt.Seconds;
+            var newPosition = currentPosition + newVelocity * dt.Seconds;
+            return (newPosition, newVelocity);
+        }
+
+        #endregion
+    }
+}
